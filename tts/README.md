41c05a19badc18b95db8cfff6f0e65784a10f2c1
N/Name <87293285+0xCurtis@users.noreply.github.com>
2023-04-09 13:12:21 +0200

    added config and some refactor

---
+++ tts/README.md
@@ -0,0 +1,25 @@
+# EXCHANGE TO VOICE
+
+Feel like a dumb clown after loosing 6 figures to some VC ?
+
+Want to feel like Warren Buffet in the 80's !
+
+Follow the instructions
+
+clone the repo
+```
+cd tts
+pip install -r requirements.txt
+python config.py
+... ...
+(do the config stuff or stick with default params)
+
+python tts.py
+```
+
+### Based on an idea by [@liquiditygoblin](https://twitter.com/liquiditygoblin)
+
+If you have any remarks/questions
+
+- [@liquiditygoblin](https://twitter.com/liquiditygoblin)
+- [@0xCurtisuke](https://twitter.com/0xCurtisuke)
--- tts/tts.py
+++ tts/tts.py
@@ -1,26 +1,29 @@
 import pyttsx3
 import pygame
 from asyncio import run
+import ccxt.base.errors
 import ccxt.pro as ccxtpro
 import datetime
 from decimal import Decimal
+import json
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+SMALL_TRADE = config['SMALL_TRADE']
+BIG_TRADE = config['BIG_TRADE']
+SYMBOL = config['SYMBOL']
+TICK_SIZE = Decimal(float(config['TICK_SIZE']))  # you could do this in code but each exchange has a diff format so cbf
+PAY_THROUGH_TICKS = int(config['PAY_THROUGH_TICKS'])
+SOUND_FILE = config['SOUND_FILE']
+EXCHANGE = config['EXCHANGE']
 
 engine = pyttsx3.init()
 pygame.init()
+pygame.mixer.music.load(SOUND_FILE)
+bonk_sound = pygame.mixer.Sound(SOUND_FILE)
 
-
-
-SMALL_TRADE = 100
-BIG_TRADE = 1000
-SYMBOL = "ARB/USDT"
-TICK_SIZE = Decimal('0.0001') # you could do this in code but each exchange has a diff format so cbf
-PAY_THROUGH_TICKS = 0
-
-
-pygame.mixer.music.load('bonk.wav')
-
-bonk_sound = pygame.mixer.Sound("bonk.wav")
-
+print("Based on an silly idea by @liquiditygoblin")
 
 def say(text):
     engine.say(text)
@@ -34,25 +37,35 @@ def bonk():
 
 def num_ticks(price, tick_size):
     return int(Decimal(price) / tick_size)
-    
+
 
 async def main():
-
     # Instantiate exchange
-    exchange = ccxtpro.binance() # you can change this to any exchange ccxt supports
-    
-
+    try:
+        class_ = getattr(ccxtpro, EXCHANGE)
+    except AttributeError:
+        print("Invalid exchange name")
+        exit(0)
+    else:
+        exchange = class_()
+    # Check if the symbol exist
+    try:
+        await exchange.watch_trades(SYMBOL)
+    except ccxt.BadSymbol:
+        print("Bad symbol {} doesn't exist on {} bozo".format(SYMBOL, exchange))
+        await exchange.close()
+        exit(84)
     while True:
         trades = await exchange.watch_trades(SYMBOL)
-        
-        time = datetime.datetime.now() 
-        
+
+        time = datetime.datetime.now()
+
         buys = []
         sells = []
-        
+
         total_buy = 0
         total_sell = 0
-        
+
         for trade in trades:
             if trade["side"] == "buy":
                 buys.append(trade['price'])
@@ -72,7 +85,7 @@ async def main():
             bonk()
             print(f"[{time}] TRADE {total_buy} BUY")
             bonk()
-        
+
         if len(sells) > 1 and abs(num_ticks(sells[0] - sells[-1], TICK_SIZE)) > PAY_THROUGH_TICKS:
             ticks = abs(num_ticks(sells[0] - sells[-1], TICK_SIZE))
             print(f"[{time}] PAY THROUGH {ticks} SELL")
@@ -80,11 +93,11 @@ async def main():
         elif total_sell > BIG_TRADE:
             say("big sell")
             print(f"[{time}] BIG TRADE {total_sell} SELL")
-            
+
         elif total_sell > SMALL_TRADE:
             print(f"[{time}] TRADE {total_sell} SELL")
             bonk()
 
-        
 
-run(main())
+if __name__ == '__main__':
+    run(main())
